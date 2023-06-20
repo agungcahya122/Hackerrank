756e4b6ef2c954e6e8f2c66373911455d1ae0f5e
Agung Cahya <agungcahya122@gmail.com>
2023-06-20 23:21:57 +0700

    refactor: add condition if put the double number

--- 1_WeekPreparationKit/LonelyInteger.js
+++ 1_WeekPreparationKit/LonelyInteger.js
@@ -1,10 +1,22 @@
 function lonelyinteger(a) {
   // Write your code here
 
+  //  ______ PUT UNIQUE NUMBER ______
   let newArr = a.filter(
     (num, index, array) => array.indexOf(num) === array.lastIndexOf(num)
   );
+
   return JSON.parse(newArr);
+
+  //  ______ PUT DOUBLE NUMBER ______
+
+  // let newArr = a.filter(
+  //   (num, index, array) => array.indexOf(num) !== array.lastIndexOf(num)
+  // );
+
+  // let result = [...new Set(newArr)];
+
+  // return result;
 }
 
-console.log(lonelyinteger([1, 1, 2]));
+console.log(lonelyinteger([1, 1, 3]));
